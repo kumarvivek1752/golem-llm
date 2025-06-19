70a0146df5a273ab6a0ae3a2fdee9c8fc8d23843
Benjamin Chibuzor-Orie <benjamincath@gmail.com>
2025-06-19 20:46:05 +0100

    fix: change retry_prompt impl for bedrock to make message resumption more fluid

--- llm/bedrock/src/lib.rs
+++ llm/bedrock/src/lib.rs
@@ -63,6 +63,58 @@ impl ExtendedGuest for BedrockComponent {
         }
     }
 
+    fn retry_prompt(
+        original_messages: &[Message],
+        partial_result: &[llm::StreamDelta],
+    ) -> Vec<Message> {
+        let mut extended_messages = Vec::new();
+        extended_messages.push(Message {
+            role: llm::Role::System,
+            name: None,
+            content: vec![
+                llm::ContentPart::Text(
+                    "You were asked the same question previously, but the response was interrupted before completion. \
+                     Please continue your response from where you left off. \
+                     Do not include the part of the response that was already seen. If the response starts with a new word and no punctuation then add a space to the beginning".to_string()),
+            ],
+        });
+        extended_messages.push(Message {
+            role: llm::Role::User,
+            name: None,
+            content: vec![llm::ContentPart::Text(
+                "Here is the original question:".to_string(),
+            )],
+        });
+        extended_messages.extend_from_slice(original_messages);
+
+        let mut partial_result_as_content = Vec::new();
+        for delta in partial_result {
+            if let Some(contents) = &delta.content {
+                partial_result_as_content.extend_from_slice(contents);
+            }
+            if let Some(tool_calls) = &delta.tool_calls {
+                for tool_call in tool_calls {
+                    partial_result_as_content.push(llm::ContentPart::Text(format!(
+                        "<tool-call id=\"{}\" name=\"{}\" arguments=\"{}\"/>",
+                        tool_call.id, tool_call.name, tool_call.arguments_json,
+                    )));
+                }
+            }
+        }
+
+        extended_messages.push(Message {
+            role: llm::Role::User,
+            name: None,
+            content: vec![llm::ContentPart::Text(
+                "Here is the partial response that was successfully received:".to_string(),
+            )]
+            .into_iter()
+            .chain(partial_result_as_content)
+            .collect(),
+        });
+        extended_messages
+    }
+
     fn subscribe(_stream: &Self::ChatStream) -> golem_rust::wasm_rpc::Pollable {
         unimplemented!()
     }
